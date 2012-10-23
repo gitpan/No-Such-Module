697ff4ce7c751dad02cd14776389a1ab48ef385e
Neil Bowers <neil@bowers.com>
2012-10-23 20:39:01 +0000

    Import of NEILB/No-Such-Module-0.01 from CPAN.
    gitpan-cpan-distribution: No-Such-Module
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/No-Such-Module-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/No/Such/Module.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+package No::Such::Module;
+
+=head1 NAME
+
+No::Such::Module - doesn't exist, you never saw it
+
+=head1 DESCRIPTION
+
+There isn't any description, because there's no such module.
+
+
+1;
